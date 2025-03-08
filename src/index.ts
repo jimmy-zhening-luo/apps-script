530f153ff5123d1f92e46bea3a3327d8e12b3cd3
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-03-07 18:06:03 -0800

    append main to all main loggers

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ function main() {
   console.log("main(): Hydrated script properties");
   // #endregion
   // #region Jobs: Gmail
-  console.log("JOB: START: Gmail: Clean Garbage");
+  console.log("main(): JOB: START: Gmail: Clean Garbage");
   cleanGarbage(properties.labels.labelGmailGarbage);
-  console.log("JOB: END: Gmail: Clean Garbage");
+  console.log("main(): JOB: END: Gmail: Clean Garbage");
   // #endregion
   // #region Jobs: Calendar
-  console.log("JOB: START: Calendar: Event Colors");
+  console.log("main(): JOB: START: Calendar: Event Colors");
   eventColors();
-  console.log("JOB: END: Calendar: Event Colors");
+  console.log("main(): JOB: END: Calendar: Event Colors");
   // #endregion
   console.log("main(): SUCCESS");
 }
