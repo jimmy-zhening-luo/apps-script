85020322be5736e9c9ff9b3189ab635fa986e21b
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-03-07 18:11:19 -0800

    hydrate events

--- src/props/index.ts
+++ src/props/index.ts
@@ -2,12 +2,16 @@ function hydrate() {
   const properties = PropertiesService.getScriptProperties(),
   labels = DefinedProperties.Labels
     .map(label => [label, properties.getProperty(label)] as const)
+    .filter((entry): entry is readonly [typeof entry[0], string] => (entry[1] ?? null) !== null),
+  events = DefinedProperties.Events
+    .map(event => [event, properties.getProperty(event)] as const)
     .filter((entry): entry is readonly [typeof entry[0], string] => (entry[1] ?? null) !== null);
 
-  if (labels.length < DefinedProperties.Labels.length)
+  if (labels.length < DefinedProperties.Labels.length || events.length < DefinedProperties.Events.length)
     throw new ReferenceError("Some expected labels are missing");
 
   return {
     labels: Object.fromEntries(labels) as Record<typeof labels[number][0], string>,
+    events: Object.fromEntries(events) as Record<typeof events[number][0], string>,
   };
 }
