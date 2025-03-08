d8af408bd3f85bf6e3080c4df2dc87631c681e53
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-03-07 18:10:22 -0800

    +initial event labels

--- src/props/props.ts
+++ src/props/props.ts
@@ -1,3 +1,9 @@
 const DefinedProperties = {
   Labels: ["labelGmailGarbage"] as const,
+  Events: [
+    "eventBuffer",
+    "eventTherapy",
+    "eventMedical",
+    "eventHaircut",
+  ] as const,
 } as const;
