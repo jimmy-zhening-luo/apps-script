133a6990b84c015a06b619361551bdcb359610fd
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-05-27 16:46:12 -0700

    -labels

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 function main() {
-  console.log("main(): BEGIN");
-
   const properties = hydrate();
 
-  console.log("main(): Hydrated script properties");
-  console.log("main(): JOB: START: Gmail: Clean Garbage");
+  console.log("main(): JOB: Start: Gmail: Clean Garbage");
   cleanGarbage(properties.labels.labelGmailGarbage);
-  console.log("main(): JOB: END: Gmail: Clean Garbage");
-  console.log("main(): JOB: START: Calendar: Event Colors");
+  console.log("main(): JOB: COMPLETE: Gmail: Clean Garbage");
+
+  console.log("main(): JOB: Start: Calendar: Event Colors");
   eventColors(
     properties.events.eventBuffer,
     properties.events.eventTherapy,
     properties.events.eventDoctor,
     properties.events.eventHaircut,
   );
-  console.log("main(): JOB: END: Calendar: Event Colors");
+  console.log("main(): JOB: COMPLETED: Calendar: Event Colors");
+
   console.log("main(): SUCCESS");
 }
