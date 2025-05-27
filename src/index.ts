eff323da159eb2d58f459cd3c4ddb36e8f3723d3
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-05-27 16:44:33 -0700

    -region tags

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 function main() {
   console.log("main(): BEGIN");
 
-  // #region Prepare
   const properties = hydrate();
 
   console.log("main(): Hydrated script properties");
-  // #endregion
-  // #region Jobs: Gmail
   console.log("main(): JOB: START: Gmail: Clean Garbage");
   cleanGarbage(properties.labels.labelGmailGarbage);
   console.log("main(): JOB: END: Gmail: Clean Garbage");
-  // #endregion
-  // #region Jobs: Calendar
   console.log("main(): JOB: START: Calendar: Event Colors");
   eventColors(
     properties.events.eventBuffer,
@@ -20,6 +15,5 @@ function main() {
     properties.events.eventHaircut,
   );
   console.log("main(): JOB: END: Calendar: Event Colors");
-  // #endregion
   console.log("main(): SUCCESS");
 }
