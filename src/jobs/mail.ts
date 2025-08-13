c771196445ccad718b471421458c05560eb84ca6
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-08-13 16:11:00 -0700

    Fix variable name in cleanGarbage function

--- src/jobs/mail.ts
+++ src/jobs/mail.ts
@@ -1,5 +1,5 @@
 function cleanGarbage(garbageTag: string) {
-  const garbage = getThreads(label);
+  const garbage = getThreads(garbageTag);
 
   console.log(`cleanGarbage: Retrieved ${garbage.length} threads tagged with: ${garbageTag}`);
 
